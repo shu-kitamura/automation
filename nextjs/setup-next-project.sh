96b1000e59ab856159c94653cbed5dc4d43e917b
shu-kitamura <shusei3316@yahoo.co.jp>
2026-02-14 14:27:36 +0900

    add jest for setup

--- nextjs/setup-next-project.sh
+++ nextjs/setup-next-project.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 
 PWD=$(pwd)
 app_name=$1
@@ -39,4 +40,48 @@ pnpm pkg set scripts.lint:fix="oxlint --fix"
 pnpm pkg set scripts.format="oxfmt --check"
 pnpm pkg set scripts.format:fix="oxfmt"
 
+# add jest
+pnpm add -D jest jest-environment-jsdom @testing-library/react @testing-library/dom @testing-library/jest-dom ts-node @types/jest
+
+# create jest.config.ts
+cat > jest.config.ts <<'TS'
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({ dir: "./" });
+
+const config: Config = {
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  clearMocks: true,
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};
+
+export default createJestConfig(config);
+TS
+
+# create jest.setup.ts
+cat > jest.setup.ts <<'TS'
+import "@testing-library/jest-dom";
+TS
+
+# add jest scripts to package.json
+pnpm pkg set scripts.test="jest"
+
+# add test example
+mkdir -p __tests__
+
+cat > __tests__/example.test.ts <<'TS'
+describe("example", () => {
+  it("1+1=2", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
+TS
+
+# add cache dir to .gitignore
+echo ".swc" >> .gitignore
+
 cd $PWD
+
+echo "Next.js (and linter, formatter, test) setup done"
